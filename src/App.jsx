0ff1c81411bee94f2b5d7b14b044ab4486f28114
rafft.zocken <rafft.zocken@gmail.com>
2024-12-28 13:15:12 +0000

    header and theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Header from "./components/header";
 
 
 
 function App() {
 
+  const [gettheme, settheme] = useState(false);
+  const [getcreate, setcreate] = useState(false);
+
+  useEffect(() => { document.body.classList.toggle("dark"); }
+
+    , [gettheme])
+
   return (
     <>
-
+      <Header gettheme settheme={settheme} setcreate={setcreate} />
     </>
   )
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,32 @@
+import { faMoon, faPaw, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../style/header.scss"
+import { faSquarePlus } from "@fortawesome/free-solid-svg-icons/faSquarePlus";
+
+export default function Header({ gettheme, settheme, setcreate }) {
+
+
+
+    return (
+        <header className={gettheme ? "dark" : null}>
+            <div>
+                <FontAwesomeIcon icon={faPaw} size="xl" />
+                <h1>TaskTiger</h1>
+            </div>
+            <div className="icons">
+                <FontAwesomeIcon
+                    icon={faSquarePlus}
+                    size="xl"
+                    color={gettheme ? "#fff" : "#000"}
+                    onClick={() => setcreate(prevState => !prevState)}
+                />
+                <FontAwesomeIcon
+                    icon={gettheme ? faMoon : faSun}
+                    size="xl"
+                    color={gettheme ? "#fff" : "#000"}
+                    onClick={() => settheme(prevState => !prevState)}
+                />
+            </div>
+        </header>
+    )
+}
