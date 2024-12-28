7e07c9c3e12360334ff5f989c6162efd22c1b4bf
rafft.zocken <rafft.zocken@gmail.com>
2024-12-28 13:22:39 +0000

    Load lazy header / placeholder Load

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import Header from "./components/header";
-
+import { Suspense, lazy, useEffect, useState } from "react";
+import Load from "./components/Load";
+const Header = lazy(() => import("./components/header"));
 
 
 function App() {
@@ -8,13 +8,13 @@ function App() {
   const [gettheme, settheme] = useState(false);
   const [getcreate, setcreate] = useState(false);
 
-  useEffect(() => { document.body.classList.toggle("dark"); }
-
-    , [gettheme])
+  useEffect(() => { document.body.classList.toggle("dark"); }, [gettheme])
 
   return (
     <>
-      <Header gettheme settheme={settheme} setcreate={setcreate} />
+      <Suspense fallback={<Load />}>
+        <Header gettheme settheme={settheme} setcreate={setcreate} />
+      </Suspense>
     </>
   )
 }
---
+++ src/components/Load.jsx
@@ -0,0 +1,5 @@
+export default function Load() {
+    return (
+        <h1>Load</h1>
+    )
+}
